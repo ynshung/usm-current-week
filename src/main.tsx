4680b2791b8d33b33e998afdec3522410b6e36ea
Young Shung <me@ynshung.com>
2025-04-21 08:50:50 +0800

    add github corner

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import GitHubCorner from './GitHubCorner.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GitHubCorner link='https://github.com/ynshung/usm-current-week' />
     <App />
   </StrictMode>,
 )
